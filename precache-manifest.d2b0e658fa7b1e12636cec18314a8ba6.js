ca025770bb9ff195160f16706d72ce86f3204fda
eganikhith <92620161+eganikhith@users.noreply.github.com>
2021-10-19 11:30:25 +0530

    Updates

---
+++ precache-manifest.d2b0e658fa7b1e12636cec18314a8ba6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f081ec4826f7debafeee6b6464e64964",
+    "url": "/Library-App/index.html"
+  },
+  {
+    "revision": "d9e4b5d94ab17ca72936",
+    "url": "/Library-App/static/css/main.01dfbae3.chunk.css"
+  },
+  {
+    "revision": "42f83c6f3055563d0672",
+    "url": "/Library-App/static/js/2.d46ef1a7.chunk.js"
+  },
+  {
+    "revision": "d9e4b5d94ab17ca72936",
+    "url": "/Library-App/static/js/main.54590a2b.chunk.js"
+  },
+  {
+    "revision": "2be05f4b89ed1375de6e",
+    "url": "/Library-App/static/js/runtime-main.d966c134.js"
+  }
+]);
